471b580b4578e1756d5e350f661e3edddf7c29b5
iBednar <p.bednarczyk@icloud.com>
2014-08-02 18:06:50 +0200

    Witaj Świecie

---
+++ WitajSwieciePoRazPiatyTests/WitajSwieciePoRazPiatyTests.m
@@ -0,0 +1,34 @@
+//
+//  WitajSwieciePoRazPiatyTests.m
+//  WitajSwieciePoRazPiatyTests
+//
+//  Created by Pawel Bednarczyk on 16.07.2014.
+//  Copyright (c) 2014 Pawel Bednarczyk. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WitajSwieciePoRazPiatyTests : XCTestCase
+
+@end
+
+@implementation WitajSwieciePoRazPiatyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
